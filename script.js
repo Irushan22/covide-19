29a6e2304929f59c989cb25aa483ded9afe8f3e9
Irushan_IT17121866 <it17121866@my.sliit.lk>
2020-11-08 21:01:51 +0530

    added new card style append data

--- script.js
+++ script.js
@@ -9,19 +9,30 @@ $(document).ready(function(){
         var local_total_cases = result.data.local_total_cases;
         var local_total_number_of_individuals_in_hospitals = result.data.local_total_number_of_individuals_in_hospitals;
         var total_pcr_testing_count = result.data.total_pcr_testing_count;
+        console.log(local_total_cases);
+        $("#local_total_cases").text(local_total_cases);
+        $("#local_total_cases").text(local_total_cases);
+        $("#local_deaths").text(local_deaths);
+        $("#local_new_deaths").text(local_new_deaths);
+        $("#total_pcr_testing_count").text(total_pcr_testing_count);
+        $("#local_active_cases").text(local_active_cases);
+        $("#local_recovered").text(local_recovered);
+        $("#suspected").text(local_total_number_of_individuals_in_hospitals);
+        // local_total_cases = $("#demo").html();
+        // document.getElementById("demo").innerHTML = local_total_cases;
       });
 
 
-    $('.count').each(function () {
-      $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 1000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+//     $('.count').each(function () {
+//       $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 1000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
 
   });
