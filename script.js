30547ff941d3604135d2ef794e27aa138801b1d9
Irushan_IT17121866 <it17121866@my.sliit.lk>
2020-11-08 21:47:10 +0530

    add live btn

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
       $.get("https://hpb.health.gov.lk/api/get-current-statistical", function(result){
         console.log(result);
+        var update_date_time = result.data.update_date_time;
         var local_active_cases = result.data.local_active_cases;
         var local_deaths = result.data.local_deaths;
         var local_new_cases = result.data.local_new_cases;
@@ -10,8 +11,9 @@ $(document).ready(function(){
         var local_total_number_of_individuals_in_hospitals = result.data.local_total_number_of_individuals_in_hospitals;
         var total_pcr_testing_count = result.data.total_pcr_testing_count;
         console.log(local_total_cases);
+        $("#update_date_time").text(update_date_time);
         $("#local_total_cases").text(local_total_cases);
-        $("#local_total_cases").text(local_total_cases);
+        $("#local_new_cases").text(local_new_cases);
         $("#local_deaths").text(local_deaths);
         $("#local_new_deaths").text(local_new_deaths);
         $("#total_pcr_testing_count").text(total_pcr_testing_count);
